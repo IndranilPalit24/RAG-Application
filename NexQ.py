66fbde6c593eff7928c5f9945ece7d033489215c
Indranil Palit <indronilpalit@outlook.com>
2024-10-11 23:16:52 +0530

    SalesData

--- NexQ.py
+++ NexQ.py
@@ -23,7 +23,7 @@ business_handler = BusinessDataHandler()
 sales_handler = SalesDataHandler('sales_data_sample.csv')
 
 # Initialize QueryHandler to manage business-related data queries
-query_handler = QueryHandler('data/sales_data_sample.csv') # Path to your mock data
+query_handler = QueryHandler('Data/sales_data_sample.csv') # Path to your mock data
                                                               
 # Load pre-trained model for question answering
 qa_model = pipeline("question-answering", model="deepset/roberta-base-squad2")
