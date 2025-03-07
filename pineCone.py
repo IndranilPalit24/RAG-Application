7f08f83d0d10c52fe1d2bd08fa60f4a09360ab89
Indranil_Palit <73847100+IndranilPalit24@users.noreply.github.com>
2025-03-07 11:17:11 +0530

    Update pineCone.py

--- pineCone.py
+++ pineCone.py
@@ -6,6 +6,6 @@ from pinecone import Pinecone
 sentence_embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 # Pinecone setup
-pinecone_client = Pinecone(api_key="<Your"_Key>")
+pinecone_client = Pinecone(api_key="<Your_API__Key>")
 business_index = pinecone_client.Index("enterprise-rag-data")
 
